caf892f0fe4fbab7f75f67e49b7ba8c3eb7ebc19
Fırat Ergül <firatergul@windowslive.com>
2021-05-23 23:38:38 +0300

    Controllers added.

--- EasyCRM.API/Controllers/AccountsController.cs
+++ EasyCRM.API/Controllers/AccountsController.cs
@@ -10,6 +10,7 @@ using EasyCRM.Utility;
 using System.Collections.Generic;
 using EasyCRM.Business.ModelHelpers;
 using EasyCRM.API.DTOs;
+using System.Linq;
 
 namespace EasyCRM.Controllers
 {
@@ -19,97 +20,23 @@ namespace EasyCRM.Controllers
     {
         private readonly IAccountManager accountManager;
         private readonly IAddressManager addressManager;
+        private readonly ICommunicationInfoManager commInfoManager;
         private readonly IMapper mapper;
 
 
-        public AccountsController(IAccountManager _accountManager,IAddressManager _addressManager, IMapper _mapper)
+        public AccountsController(IAccountManager _accountManager,
+                                  IAddressManager _addressManager,
+                                  ICommunicationInfoManager _commInfoManager,
+                                  IMapper _mapper)
         {
             accountManager = _accountManager;
             addressManager = _addressManager;
             mapper = _mapper;
+            commInfoManager = _commInfoManager;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> SearchAccounts([FromQuery] AccountParams accountParams)
-        {
-            try
-            {
-                var accounts = await accountManager.SearchAccounts(accountParams);
-
-                if(accounts != null )
-                {
-                    PagedList<AccountToReturnDTO> accountToReturns = new PagedList<AccountToReturnDTO>();
-
-                    foreach(var acc in accounts)
-                    {
-                        var contacts = acc.Contacts;
-                        List<ContactReturnDTO> contactsToReturn = new List<ContactReturnDTO>();
-                        
-                        foreach(var c in contacts)
-                        {
-                            ContactReturnDTO contact = new ContactReturnDTO()
-                            {
-                                ContactId=c.ContactId,
-                                FirstName = c.FirstName,
-                                MiddleName = c.MiddleName,
-                                LastName= c.LastName,
-                                NameTitle= c.NameTitle,
-                                SelectedAddressId=(int)c.SelectedAddressId,
-                                AccountId=c.AccountId.Value,
-                                Notes = c.Notes
-                            };
-                            contactsToReturn.Add(contact);
-                        }
-
-                        AccountToReturnDTO accountsToReturn = new AccountToReturnDTO
-                        {
-                            AccountId=acc.AccountId,
-                            OrganizationName= acc.OrganizationName,
-                            AccountType=acc.AccountType.Name,
-                            Description= acc.Description,
-                            CreatedAt= acc.CreatedAt,
-                            UpdatedAt =acc.UpdatedAt,
-                            Contacts = contactsToReturn
-                        };
-                        accountToReturns.Add(accountsToReturn);
-                    }
-
-                    Response.AddPagination(accountToReturns.CurrentPage, accountToReturns.PageSize, accountToReturns.TotalCount, accountToReturns.TotalPages);
-                    return Ok(accountToReturns);
-                }
-                return NotFound("Not Found!");
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-
-        }
-
-        [HttpGet("{id}", Name = "GetAccount")]
-        public async Task<IActionResult> GetAccountById(int id)
-        {
-            try
-            {
-                Account acc= await accountManager.GetAccount(id);
-                if(acc != null)
-                {
-
-                    var accountToReturn = mapper.Map<AccountToReturnDTO>(acc);
-                    return Ok(accountToReturn);
-                }
-
-                return NotFound("Sorry, Account Not Found!");
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-
-        }
-
-        [HttpPost("/new")]
+        #region Account Base Methods
+        [HttpPost]
         public async Task<IActionResult> CreateNewAccount([FromBody] AccountCreationDTO accountForCreation)
         {
             try
@@ -121,8 +48,8 @@ namespace EasyCRM.Controllers
                 {
                     var accountToReturn = mapper.Map<AccountToReturnDTO>(acc);
                     return CreatedAtRoute("GetAccount", new { Id = accountToReturn.AccountId }, accountToReturn);
+                    
                 }
-
                 return BadRequest("Given Account could not be added.");
             }
             catch (Exception)
@@ -131,19 +58,54 @@ namespace EasyCRM.Controllers
             }
         }
 
+        [HttpGet("{id}", Name = "GetAccount")]
+        public async Task<IActionResult> GetAccountById(int id)
+        {
+            try
+            {
+                Account acc = await accountManager.GetAccount(id);
+                if (acc == null)
+                {
+                    return NotFound("Sorry, Account Not Found!");
+                }
+                var accountGroups = acc.AccountGroups.ToList();
+                
+                List<Group> groups = new List<Group>();
+
+                foreach(var ag in accountGroups)
+                {
+                    groups.Add(ag.Group);
+                }
+
+                
+                var groupsToReturn = mapper.Map<List<GroupReturnDTO>>(groups);
+               
+                var accountToReturn = mapper.Map<AccountToReturnDTO>(acc);
+                accountToReturn.Groups = groupsToReturn;
+                
+                return Ok(accountToReturn);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
         [HttpPut("{id}")]
-        public async Task<IActionResult> EditAccountInfo(int id, [FromBody]AccountEditDTO accountEditDTO)
+        public async Task<IActionResult> EditAccountInfo(int id, [FromBody] AccountEditDTO accountEditDTO)
         {
             try
             {
                 var account = mapper.Map<Account>(accountEditDTO);
+
                 var isEdited = await accountManager.EditAccount(id, account);
-                if(isEdited == true)
+                if (isEdited == true)
                 {
                     return NoContent();
                 }
                 return BadRequest();
-                
+
             }
             catch (Exception)
             {
@@ -157,22 +119,106 @@ namespace EasyCRM.Controllers
             try
             {
                 var account = await accountManager.GetAccount(id);
-                if(account != null)
-                {
-                    var isDeleted = await accountManager.DeleteAccount(id);
-                    if (isDeleted == true)
-                    {
-                        return NoContent();
-                    }
-                    return BadRequest("Account could not be deleted!");
-                }
-                else
+                if (account == null)
                 {
                     return NotFound("No account found with given id!");
-                }               
+                }
+
+                var isDeleted = await accountManager.DeleteAccount(id);
+                if (isDeleted != true)
+                {
+                    return BadRequest("Account could not be deleted!");
+                }
+                return NoContent();
+
             }
             catch (Exception)
             {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchAccounts([FromQuery] AccountParams accountParams)
+        {
+            try
+            {
+                var accounts = await accountManager.SearchAccounts(accountParams);
+
+                if (accounts == null)
+                {
+                    return NotFound("Not Found!");
+                }
+                
+                PagedList<AccountToReturnDTO> accountToReturns = new PagedList<AccountToReturnDTO>();
+
+                foreach(var acc in accounts)
+                {
+                    var contacts = acc.Contacts;
+                    List<ContactReturnDTO> contactsToReturn = new List<ContactReturnDTO>();
+                        
+                    foreach(var c in contacts)
+                    {
+                        ContactReturnDTO contact = new ContactReturnDTO()
+                        {
+                            ContactId=c.ContactId,
+                            FirstName = c.FirstName,
+                            MiddleName = c.MiddleName,
+                            LastName= c.LastName,
+                            NameTitle= c.NameTitle,
+                            SelectedAddressId=c.SelectedAddressId,
+                            AccountId=c.AccountId.Value,
+                            Notes = c.Notes
+                        };
+                        contactsToReturn.Add(contact);
+                    }
+
+                    AccountToReturnDTO accountsToReturn = new AccountToReturnDTO
+                    {
+                        AccountId=acc.AccountId,
+                        OrganizationName= acc.OrganizationName,
+                        AccountType=acc.AccountType.Name,
+                        Description= acc.Description,
+                        CreatedAt= acc.CreatedAt,
+                        UpdatedAt =acc.UpdatedAt,
+                        Contacts = contactsToReturn
+                    };
+                    accountToReturns.Add(accountsToReturn);
+                }
+
+                Response.AddPagination(accountToReturns.CurrentPage, accountToReturns.PageSize, accountToReturns.TotalCount, accountToReturns.TotalPages);
+                return Ok(accountToReturns);
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+        #endregion
+
+        #region Account's Address Methods
+
+        [HttpGet("{accountId}/addresses/{addressId}")]
+        public async Task<IActionResult> GetAccountAddressByAddressId(int accountId, int addressId)
+        {
+            try
+            {
+
+                var address = await addressManager.GetAddress(addressId,accountId);
+                if(address != null)
+                {
+                    var addressToReturn = mapper.Map<AddressReturnDTO>(address);
+                    return Ok(addressToReturn);
+                }
+
+                return NotFound();
+            }
+            catch (Exception)
+            {
+
                 throw;
             }
         }
@@ -182,27 +228,22 @@ namespace EasyCRM.Controllers
         {
             try
             {
-                var acc = await accountManager.GetAccount(addressCreationDTO.AccountId);
+                var acc = await accountManager.GetAccount((int)addressCreationDTO.AccountId);
 
-                if (acc != null)
+                if (acc == null)
                 {
-                    var address = mapper.Map<Address>(addressCreationDTO);
-                    var result = await addressManager.CreateAddress(address);
-
-                    if (result == true)
-                    {
-                        return NoContent();
-                    }
-                    else
-                    {
-                        throw new Exception("Address could not added!");
-                    }
-
+                    return BadRequest("Invalid Account Id!");
                 }
-                else
+
+                var address = mapper.Map<Address>(addressCreationDTO);
+                var result = await addressManager.CreateAddress(address);
+
+                if (result == true)
                 {
-                    throw new Exception("Invalid Account Id!");
+                    return NoContent();
+                    // return Created();
                 }
+                return BadRequest();
             }
             catch (Exception)
             {
@@ -211,17 +252,114 @@ namespace EasyCRM.Controllers
             }
         }
 
-        //[HttpPost("{accountId}/addCommInfo")]
-        //public async Task<IActionResult> AddCommInfo([FromBody] CommInfoParams commInfoParams)
-        //{
-        //    throw new NotImplementedException();
+        [HttpPut("{accountId}/addresses/{addressId}")]
+        public async Task<IActionResult> EditAccountAddress(int accountId, int addressId, [FromBody] AddressEditDTO addressEditDTO)
+        {
+            try
+            {
+                var acc = await accountManager.GetAccount(accountId);
+                if (acc != null)
+                {
+                    var address = mapper.Map<Address>(addressEditDTO);
+                    var result = await addressManager.EditAddress(addressId,address);
+                    if(result == true)
+                    {
+                        return NoContent();
+                    }
+                   
+                }
 
-        //}
+                return BadRequest();
+            }
+            catch (Exception)
+            {
 
-        //[HttpDelete("{accountId}/deleteAddressInfo/{addressId}")]
-        //public async Task<IActionResult> DeleteAddressInfo(int accountId, int addressId)
+                throw;
+            }
+        }
+
+        [HttpDelete("{accountId}/addresses/{addressId}")]
+        public async Task<IActionResult> DeleteAddressInfo(int accountId, int addressId)
+        {
+            try
+            {
+                var acc = await accountManager.GetAccount(accountId);
+
+                if (acc == null)
+                {
+                    return NotFound("No account found with given id!");
+                }
+
+
+                var adr = await addressManager.GetAddress(addressId);
+
+                if (adr == null)
+                {
+                    return NotFound("No address found with given id!");
+                }
+
+                if (adr.IsMain == true)
+                {
+                    return BadRequest("Could not delete primary address.");
+                }
+
+                var result = await addressManager.DeleteAddress(addressId);
+                if (result == true)
+                {
+                    return NoContent();
+                }
+
+                return BadRequest("");
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        //public async Task<IActionResult> SearchAccountAddress([FromQuery] AddressParams accountAddressParams)
         //{
-        //    throw new NotImplementedException();
+        //    return null;
         //}
+        
+        #endregion
+
+        #region Account's CommInfo Methods
+        [HttpPost("{accountId}/addCommInfo")]
+        public async Task<IActionResult> CreateAccountCommInfo([FromBody] CommInfoCreationDTO commInfoCreationDTO)
+        {
+            try
+            {
+                if(commInfoCreationDTO.AccountId !=null && commInfoCreationDTO.AccountId > 0) 
+                {
+                    var acc = await accountManager.GetAccount((int)commInfoCreationDTO.AccountId);
+
+                    if (acc != null) 
+                    {
+                        var commInfo = mapper.Map<CommunicationInfo>(commInfoCreationDTO);
+
+                        var result = await commInfoManager.CreateCommunicationInfo(commInfo);
+
+                        if (result == true)
+                        {
+                            return NoContent();
+                        }
+
+                    }
+                    
+                }
+                return BadRequest("Invalid Account Id!");
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+        #endregion
     }
 }
--- EasyCRM.API/Controllers/ContactController.cs
+++ EasyCRM.API/Controllers/ContactController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using EasyCRM.API.DTOs;
 using EasyCRM.Business.Managers.Abstract;
+using EasyCRM.Business.ModelHelpers;
 using EasyCRM.Entity.Models;
+using EasyCRM.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -16,36 +18,58 @@ namespace EasyCRM.Controllers
         private readonly IContactManager contactManager;
         private readonly IMapper mapper;
 
-
-
         public ContactController(IContactManager _contactManager, IMapper _mapper)
         {
             contactManager = _contactManager;
             mapper = _mapper;
-
         }
 
-        //[HttpGet]
-        //public async Task<IActionResult> SearchContacts([FromQuery] ContactParams contactParams)
-        //{
-        //    try
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-        //    catch (Exception)
-        //    {
+        [HttpGet]
+        public async Task<IActionResult> SearchContacts([FromQuery] ContactParams contactParams)
+        {
+            try
+            {
+                var contacts = await contactManager.SearchContacts(contactParams);
 
-        //        throw;
-        //    }
-        //}
+                if(contacts != null)
+                {
+                    PagedList<ContactReturnDTO> contactsToReturn = new PagedList<ContactReturnDTO>();
+
+                    foreach(var c in contacts)
+                    {
+                        ContactReturnDTO cr = new ContactReturnDTO
+                        {
+                            AccountId = c.AccountId,
+                            ContactId = c.ContactId,
+                            FirstName = c.FirstName,
+                            LastName = c.LastName,
+                            MiddleName = c.MiddleName,
+                            NameTitle = c.NameTitle,
+                            Notes = c.Notes,
+                            SelectedAddressId = c.SelectedAddressId
+                        };
+                        contactsToReturn.Add(cr);
+                    }
+
+                    Response.AddPagination(contactsToReturn.CurrentPage,contactsToReturn.PageSize,contactsToReturn.TotalCount,contactsToReturn.TotalPages);
+                    return Ok(contactsToReturn);
+                }
+                return NotFound();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
 
         [HttpGet("{id}", Name = "GetContact")]
-        public async Task<IActionResult> GetContactById(int contactId)
+        public async Task<IActionResult> GetContactById(int id)
         {
 
             try
             {
-                var c = await contactManager.GetContact(contactId);
+                var c = await contactManager.GetContact(id);
 
                 if (c != null)
                 {
@@ -62,7 +86,7 @@ namespace EasyCRM.Controllers
 
         }
 
-        [HttpPost]
+        [HttpPost("new")]
         public async Task<IActionResult> CreateNewContact([FromBody] ContactCreationDTO contactCreationDTO)
         {
             try
@@ -74,7 +98,7 @@ namespace EasyCRM.Controllers
                 if (result == true)
                 {
                     var contactToReturn = mapper.Map<ContactReturnDTO>(contact);
-                    return CreatedAtRoute("GetContact", new { ContactId = contactToReturn.ContactId }, contactToReturn);
+                    return CreatedAtRoute("GetContact", new { Id = contactToReturn.ContactId }, contactToReturn);
                 }
 
                 return BadRequest();
@@ -86,38 +110,54 @@ namespace EasyCRM.Controllers
             }
         }
 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditContact([FromBody] ContactEditDTO contactEditDTO, int id)
+        {
+            try
+            {
+                var contactAsInputParam = mapper.Map<Contact>(contactEditDTO);
 
+                var result = await contactManager.EditContact(id, contactAsInputParam);
 
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> EditContact([FromBody]ContactEditDTO contactEditDTO, int id) 
-        //{
-        //    try
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-        //    catch (Exception)
-        //    {
-        //        throw;
-        //    }
-        //    throw new NotImplementedException();
-        //}
+                var contact = await contactManager.GetContact(id);
 
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteContact(int id)
-        //{
+                if (result == true)
+                {
+                    var contactToReturn = mapper.Map<ContactReturnDTO>(contact);
+                    return CreatedAtRoute("GetContact", new { id = contact.ContactId }, contactToReturn);
+                }
 
-        //    try
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-        //    catch (Exception)
-        //    {
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            throw new NotImplementedException();
+        }
 
-        //        throw;
-        //    }
-        //}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContact(int id)
+        {
 
-        [HttpPost("/connectAddress")]
+            try
+            {
+                var result = await contactManager.DeleteContact(id);
+                if (result == true)
+                {
+                    return NoContent();
+                }
+
+                return BadRequest("Contact could not deleted!");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpPost("connectAddress")]
         public async Task<IActionResult> ConnectAddressToContact(int contactId, int addressId)
         {
             try
